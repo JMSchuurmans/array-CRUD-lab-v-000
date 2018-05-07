cab16daf9b0c5ae43d76d0da7141669a9843b172
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-07 19:52:30 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["Element 1", " Element 2", "Element 3", "Element 4"]
-  array.pop("Element 4")
+  element_4 = array.pop
 end
 
 def remove_element_from_start_of_array(array)
