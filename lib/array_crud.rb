0bf713dfa4cf7bb3513e953f1f58ae907dca46a8
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-07 19:40:31 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array = ["Element 1", " Element 2", "Element 3", "Element 4"]
+  element 4.push
 end
 
 def add_element_to_start_of_array(array, element)
