7366512e7001d38c45168272a548d34203bda56e
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-07 19:49:36 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["Element 1", " Element 2", "Element 3", "Element 4"]
+  array = ["Element 1", " Element 2", "Element 3", "Element 4", "arrays!"]
   array.pop("arrays!")
 end
 
